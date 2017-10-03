3fac8e5b3e6510bb444b6e39c4cccbf2156c3954
sarahneely <sporter1990@hotmail.com>
2017-10-03 15:25:43 -0700

    fixed the typo in the url and added a div and box around the img tag in html

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,6 @@
 let number = Math.ceil(Math.random() *20);
 console.log(number);
 
-let switchImg = document.getElementById('image').style.backgroundImage = "url('../img/magic*ball_' + number + '.png')";
+let switchImg = document.getElementById('image').style.backgroundImage = "url('../img/magic8ball_' + number + '.png')";
 
 document.getElementById('click_button').addEventListener('click', switchImg);
