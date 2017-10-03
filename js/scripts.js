ea86cbb8bb41c6b48571331431fbc7dc72285275
sarahneely <sporter1990@hotmail.com>
2017-10-02 18:56:14 -0700

    added html elements, js, and empty styles in the css files

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 // target your button and assign an ask function that outputs a random image to your page
+
+let number = Math.ceil(Math.random() *20);
+console.log(number);
+
+let switchImg = document.getElementById('image').style.backgroundImage = "url('../img/magic*ball_' + number + '.png')";
+ document.getElementById('click_button').addEventListener('click', switchImg);
