d4283f5dc1ed70140ffcebfcbac3140bdc78210b
sarahneely <sporter1990@hotmail.com>
2017-10-03 17:54:09 -0700

    fixed the code! It works now!

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 // target your button and assign an ask function that outputs a random image to your page
 
-let number = Math.ceil(Math.random() *20);
-console.log(number);
 
-let switchImg = document.getElementById('image').style.backgroundImage = "url('../img/magic8ball_' + number + '.png')";
+
+function switchImg (){
+  let number = Math.ceil(Math.random() *20);
+  console.log(number);
+  document.querySelector('.image_box').style.backgroundImage = "url('img/magic8ball_" + number + ".png')";
+}
 
 document.getElementById('click_button').addEventListener('click', switchImg);
